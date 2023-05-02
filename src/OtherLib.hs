86431e941656e152bb5c6287b60b1254cff290c0
Jens Danbolt <jens_bolt92@hotmail.com>
2023-05-02 18:47:20 +0200

    added three simple functions

--- src/OtherLib.hs
+++ src/OtherLib.hs
@@ -2,3 +2,12 @@ module OtherLib where
 
 otherFunc :: String -> Int
 otherFunc str = length str
+
+double :: Int -> Int
+double int = int + int 
+
+quadruple :: Int -> Int
+quadruple x = double( double x )
+
+halfDouble :: Float -> Float
+halfDouble x = x * (sqrt 2)
