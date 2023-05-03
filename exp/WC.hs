fc0e319a9a892a7268f7d16ce161c94db03fc872
Jens Danbolt <jens_bolt92@hotmail.com>
2023-05-03 15:30:12 +0200

    Factored out calculations to separate function

--- exp/WC.hs
+++ exp/WC.hs
@@ -1,6 +1,10 @@
 main = interact outPut
     where 
         outPut input = show (sumWordLengths input) 
-        sumWordLengths input = sum (allWordLengths input)
+        
+
+
+sumWordLengths input = sum (allWordLengths input)
+    where
         allWordLengths input = map length (wordList input)
         wordList input = words input
